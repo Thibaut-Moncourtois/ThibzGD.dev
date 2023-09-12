7912e3028c466f45d309cf19867cd955d0ce5a87
Thibaut <thibaut.moncourtois@gmail.com>
2023-09-12 16:50:39 +0200

    Création du bouton DarkMode

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,28 @@
-import logo from '../../assets/logo.svg';
-
+import DarkMode from '../DarkMode/DarkMode';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title> ThibzGD Website</title>
+        <meta name="description" />
       </header>
+      <main>
+        <section className="min-h-screen">
+          <nav>
+            <h1>Developed by ThibzGD</h1>
+            <ul>
+              <li>
+                <DarkMode />
+              </li>
+              <li>
+                <a href="/resume">My Resume</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/DarkMode/DarkMode.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
+function DarkMode() {
+  return <FontAwesomeIcon icon={faMoon} />;
+}
+
+export default DarkMode;
