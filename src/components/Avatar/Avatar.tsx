122f754dfe3429fe59e24b76195c64c0037ec282
Thibaut <thibaut.moncourtois@gmail.com>
2023-09-12 20:46:16 +0200

    bg gradient de l'avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,8 +2,8 @@ import Avatar1 from '../../assets/avatar-1.png';
 
 function Avatar() {
   return (
-    <div className="avatar flex justify-center">
-      <img className="w-10 " src={Avatar1} alt="avatar" />
+    <div className="avatar flex justify-center relative bg-gradient-to-b from-teal-500">
+      <img src={Avatar1} alt="avatar" />
     </div>
   );
 }
